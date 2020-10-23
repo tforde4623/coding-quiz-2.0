b45f8aca8c824f638071603e621b348e64a90848
tt4623 <sltebr33zy@gmail.com>
2020-10-22 22:39:36 -0500

    added some more questions

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -6,4 +6,16 @@ let questions = [{
     'question': 'What language is NOT commonly used in FRONT-END web developement? HINT: Databases are normally used in the back-end.',
     'answers': ['CSS', 'HTML', 'SQL', 'JavaScript'],
     'correctAnswer': 'SQL'
+}, {
+    'question': 'Which example of code given, is NOT a method or a function?',
+    'answers': ['push()', 'sort()', 'string', 'log()'],
+    'correctAnswer': 'string'
+}, {
+    'question': 'What is the abbreviated name for "hyper-text markup language"?',
+    'answers': ['XML', 'CSS', 'HTML', 'HML'],
+    'correctAnswer': 'ALOT'
+}, {
+    'question': 'What does CSS stand for? HINT: c-s-s.',
+    'answers': ['Extensible Markup Language', 'Cascading Style Sheets', 'Falling Style Sheets', 'C++'],
+    'correctAnswer': 'Cascading Style Sheets'
 }];
