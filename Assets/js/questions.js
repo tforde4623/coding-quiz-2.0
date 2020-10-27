3ddc7f7a7791cc299b369dc8e34b036d2564d87a
tt4623 <sltebr33zy@gmail.com>
2020-10-26 22:19:39 -0500

    changed a question

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,21 +1,38 @@
-let questions = [{
-    'question': 'What is the worst part of JavaScript?',
-    'answers': ['Nothing, its great!', 'Syntax', 'For-loops', 'Switch statements'],
-    'correctAnswer': 'Syntax'
-}, {
-    'question': 'What language is NOT commonly used in FRONT-END web developement? HINT: Databases are normally used in the back-end.',
-    'answers': ['CSS', 'HTML', 'SQL', 'JavaScript'],
-    'correctAnswer': 'SQL'
-}, {
-    'question': 'Which example of code given, is NOT a method or a function?',
-    'answers': ['push()', 'sort()', 'string', 'log()'],
-    'correctAnswer': 'string'
-}, {
-    'question': 'What is the abbreviated name for "hyper-text markup language"?',
-    'answers': ['XML', 'CSS', 'HTML', 'HML'],
-    'correctAnswer': 'ALOT'
-}, {
-    'question': 'What does CSS stand for? HINT: c-s-s.',
-    'answers': ['Extensible Markup Language', 'Cascading Style Sheets', 'Falling Style Sheets', 'C++'],
-    'correctAnswer': 'Cascading Style Sheets'
-}];
+let questions = [
+  {
+    question: "What is the worst part of JavaScript?",
+    answers: [
+      "Nothing, its great!",
+      "Syntax",
+      "For-loops",
+      "Switch statements",
+    ],
+    correctAnswer: "Syntax",
+  },
+  {
+    question:
+      "What language is NOT commonly used in FRONT-END web developement? HINT: Databases are normally used in the back-end.",
+    answers: ["CSS", "HTML", "SQL", "JavaScript"],
+    correctAnswer: "SQL",
+  },
+  {
+    question: "Which example of code given, is NOT a method or a function?",
+    answers: ["push()", "sort()", "string", "log()"],
+    correctAnswer: "string",
+  },
+  {
+    question: 'What is the abbreviated name for "hyper-text markup language"?',
+    answers: ["XML", "CSS", "HTML", "HML"],
+    correctAnswer: "ALOT",
+  },
+  {
+    question: "What does CSS stand for? HINT: c-s-s.",
+    answers: [
+      "Extensible Markup Language",
+      "Cascading Style Sheets",
+      "Falling Style Sheets",
+      "C Plus Plus",
+    ],
+    correctAnswer: "Cascading Style Sheets",
+  },
+];
